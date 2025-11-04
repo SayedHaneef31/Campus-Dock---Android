8fff2179d331d9985d5c0411eef678800c8b2063
Sayed Haneef <sayedhaneef31@gmail.com>
2025-11-04 20:33:41 +0530

    Changed overall UI

--- app/src/main/java/com/sayed/campusdock/API/RetrofitClient.kt
+++ app/src/main/java/com/sayed/campusdock/API/RetrofitClient.kt
@@ -13,6 +13,7 @@ object RetrofitClient {
     //    private const val BASE_URL= "https://api.campusdock.live/"
 //    private const val BASE_URL= "http:/192.168.29.10:8081/"
     //private const val BASE_URL= "http://172.16.45.158:8081/"      //kc wifi
+    // TODO: Change
     private const val BASE_URL= "http://192.168.1.33:8081/"
 
     val instance: ApiService by lazy {
